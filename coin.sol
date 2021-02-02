d33cdb8b3bb38970f580f02fdb6a51f65230e890
brokenpie <78371524+brokenpie@users.noreply.github.com>
2021-02-02 11:31:37 +0530

    Update coin.sol

--- coin.sol
+++ coin.sol
@@ -1,5 +1,5 @@
-pragma solidity ^0.4.24;
-
+pragma solidity ^0.4.16;
+contract Token {
 // ----------------------------------------------------------------------------
 // FUT ERC20 Token
 //
@@ -11,170 +11,135 @@ pragma solidity ^0.4.24;
 //
 //
 //
-// (c) by Juan Cruz Martinez & Haren Sarma.
+// (c) by Haren Sarma.
 // ----------------------------------------------------------------------------
+/// @return total amount of tokens
+function totalSupply() constant returns (uint256 supply) {}
 
+/// @param _owner The address from which the balance will be retrieved
+/// @return The balance
+function balanceOf(address _owner) constant returns (uint256 balance) {}
 
-// ----------------------------------------------------------------------------
-// Lib: Safe Math
-// ----------------------------------------------------------------------------
-contract SafeMath {
+/// @notice send `_value` token to `_to` from `msg.sender`
+/// @param _to The address of the recipient
+/// @param _value The amount of token to be transferred
+/// @return Whether the transfer was successful or not
+function transfer(address _to, uint256 _value) returns (bool success) {}
 
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
-    }
+/// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+/// @param _from The address of the sender
+/// @param _to The address of the recipient
+/// @param _value The amount of token to be transferred
+/// @return Whether the transfer was successful or not
+function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
 
-    function safeSub(uint a, uint b) public pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
-    }
+/// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+/// @param _spender The address of the account able to transfer the tokens
+/// @param _value The amount of wei to be approved for transfer
+/// @return Whether the approval was successful or not
+function approve(address _spender, uint256 _value) returns (bool success) {}
 
-    function safeMul(uint a, uint b) public pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
+/// @param _owner The address of the account owning tokens
+/// @param _spender The address of the account able to transfer the tokens
+/// @return Amount of remaining tokens allowed to spent
+function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
 
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
-    }
+event Transfer(address indexed _from, address indexed _to, uint256 _value);
+event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
 }
 
 
-contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+contract StandardToken is Token {
+
+function transfer(address _to, uint256 _value) returns (bool success) {
+//Default assumes totalSupply can't be over max (2^256 - 1).
+//If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+//Replace the if with this one instead.
+//if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+if (balances[msg.sender] >= _value && _value > 0) {
+balances[msg.sender] -= _value;
+balances[_to] += _value;
+Transfer(msg.sender, _to, _value);
+return true;
+} else { return false; }
+}
+
+function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+//same as above. Replace this line with the following if you want to protect against wrapping uints.
+//if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+balances[_to] += _value;
+balances[_from] -= _value;
+allowed[_from][msg.sender] -= _value;
+Transfer(_from, _to, _value);
+return true;
+} else { return false; }
+}
+
+function balanceOf(address _owner) constant returns (uint256 balance) {
+return balances[_owner];
+}
+
+function approve(address _spender, uint256 _value) returns (bool success) {
+allowed[msg.sender][_spender] = _value;
+Approval(msg.sender, _spender, _value);
+return true;
+}
+
+function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+return allowed[_owner][_spender];
+}
+
+mapping (address => uint256) balances;
+mapping (address => mapping (address => uint256)) allowed;
+uint256 public totalSupply;
 }
 
 
-/**
-Contract function to receive approval and execute function in one call
+//name this contract whatever you'd like
+contract ERC20Token is StandardToken {
 
-Borrowed from MiniMeToken
+function () {
+//if ether is sent to this address, send it back.
+throw;
+}
+
+/* Public variables of the token */
+
+/*
+NOTE:
+The following variables are OPTIONAL vanities. One does not have to include them.
+They allow one to customise the token contract & in no way influences the core functionality.
+Some wallets/interfaces might not even bother to look at this information.
 */
-contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+string public name;                   
+uint8 public decimals;                
+string public symbol;                 
+string public version = 'B1.0';       
+
+
+//make sure this function name matches the contract name above. So if you're token is called TutorialToken, make sure the //contract name above is also TutorialToken instead of ERC20Token
+
+function ERC20Token(
+) {
+balances[msg.sender] = 10000000000;               // Give the creator all initial tokens (100000 for example)
+totalSupply = 10000000000;                        // Update total supply (100000 for example)
+name = "Future Crypto Coin";                                   // Set the name for display purposes
+decimals = 18;                            // Amount of decimals for display purposes
+symbol = "FUT";                               // Set the symbol for display purposes
 }
 
-/**
-ERC20 Token, with the addition of symbol, name and decimals and assisted token transfers
-*/
-contract FUTCoin is ERC20Interface, SafeMath {
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint public _totalSupply;
+/* Approves and then calls the receiving contract */
+function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+allowed[msg.sender][_spender] = _value;
+Approval(msg.sender, _spender, _value);
 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
-
-
-    // ------------------------------------------------------------------------
-    // Constructor
-    // ------------------------------------------------------------------------
-    constructor() public {
-        symbol = "FUT";
-        name = "Future Crypto Coin";
-        decimals = 18;
-        _totalSupply = 10000000000;
-        balances[0x02DEA85397EF756307F9751693872d54d0B75A2c] = _totalSupply;
-        emit Transfer(address(0), 0x02DEA85397EF756307F9751693872d54d0B75A2c, _totalSupply);
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Total supply
-    // ------------------------------------------------------------------------
-    function totalSupply() public constant returns (uint) {
-        return _totalSupply  - balances[address(0)];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Get the token balance for account tokenOwner
-    // ------------------------------------------------------------------------
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
-        return balances[tokenOwner];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Transfer the balance from token owner's account to to account
-    // - Owner's account must have sufficient balance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Token owner can approve for spender to transferFrom(...) tokens
-    // from the token owner's account
-    //
-    // ------------------------------------------------------------------------
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Transfer tokens from the from account to the to account
-    // 
-    // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the from account and
-    // - From account must have sufficient balance to transfer
-    // - Spender must have sufficient allowance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = safeSub(balances[from], tokens);
-        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(from, to, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Returns the amount of tokens approved by the owner that can be
-    // transferred to the spender's account
-    // ------------------------------------------------------------------------
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Token owner can approve for spender to transferFrom(...) tokens
-    // from the token owner's account. The spender contract function
-    // receiveApproval(...) is then executed
-    // ------------------------------------------------------------------------
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Don't accept ETH
-    // ------------------------------------------------------------------------
-    function () public payable {
-        revert();
-    }
+//call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+//receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+//it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
+return true;
+}
 }
